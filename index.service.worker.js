8ad0e5dfb9b29b18bb4b4a54af62ab663f7f7726
DasBeanster <158359443+DasBeanster@users.noreply.github.com>
2025-05-06 11:03:08 -0400

    Fixed win screen bug

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746506063|1799770953';
+const CACHE_VERSION = '1746543661|149697215';
 /** @type {string} */
 const CACHE_PREFIX = 'Danny-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
