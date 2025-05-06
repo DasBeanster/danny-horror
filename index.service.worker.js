d2c8b08d7125a3a82447b36b5a7968559858d8b5
DasBeanster <tylerh57911@gmail.com>
2025-05-05 23:28:00 -0400

    Minor fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746501767|778012373';
+const CACHE_VERSION = '1746502064|1074474137';
 /** @type {string} */
 const CACHE_PREFIX = 'Danny-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
